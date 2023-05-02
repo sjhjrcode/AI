6fd7cbab44eb8051b79c5e1fbe1071ae09f837ef
sjhjrcode <Steven.J.Howell.Code@gmail.com>
2023-05-01 23:30:53 -0500

    project update

---
+++ Projectv2/GODEL/GODEL.py
@@ -0,0 +1,421 @@
+from pyllamacpp.model import Model
+import speech_recognition as sr
+# import required libraries
+import sounddevice as sd
+from scipy.io.wavfile import write
+import wavio as wv
+from pathlib import Path
+import re
+import glob
+import pandas as pd
+import pylangacq as pla
+import torch
+import math, random
+from IPython.display import Audio
+import torchaudio
+from torchaudio import transforms
+import pandas as pd
+from pathlib import Path
+from torch.utils.data import DataLoader, Dataset, random_split
+import torch.nn.functional as F
+from torch.nn import init
+import matplotlib.pyplot as plt
+import mimetypes
+import os
+from pathlib import Path
+import re
+import glob
+import pylangacq as pla
+import openpyxl
+import numpy as np
+import csv
+from torch import nn
+import shutil
+from pydub import AudioSegment
+
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+import pyttsx3
+engine = pyttsx3.init()
+rate = engine.getProperty('rate')   # getting details of current speaking rate
+print (rate)                        #printing current voice rate
+engine.setProperty('rate', 175)
+voices = engine.getProperty('voices')       #getting details of current voice
+#print(voices)
+#engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
+#engine.setProperty('voice', voices[1].id) 
+tokenizer = AutoTokenizer.from_pretrained("microsoft/GODEL-v1_1-large-seq2seq")
+model = AutoModelForSeq2SeqLM.from_pretrained("microsoft/GODEL-v1_1-large-seq2seq")
+def generate(instruction, knowledge, dialog):
+    if knowledge != '':
+        knowledge = '[KNOWLEDGE] ' + knowledge
+    dialog = ' EOS '.join(dialog)
+    query = f"{instruction} [CONTEXT] {dialog} {knowledge}"
+    input_ids = tokenizer(f"{query}", return_tensors="pt").input_ids
+    outputs = model.generate(input_ids, max_length=128, min_length=8, top_p=0.9, do_sample=True)
+    output = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return output
+# Instruction for a chitchat task
+instruction = f'Instruction: given a dialog context, you need to continue a conversation with an individual.'
+# Leave the knowldge empty
+knowledge = ''
+'''
+dialog = [
+    'Does money buy happiness?',
+    'It is a question. Money buys you a lot of things, but not enough to buy happiness.',
+    'What is the best way to buy happiness ?'
+]
+print(dialog)
+response = generate(instruction, knowledge, dialog)
+print(response)
+dialog = ["How are you?"]
+print(dialog)
+response = generate(instruction, knowledge, dialog)
+print(response)
+dialog = [
+    'Does money buy happiness?',
+    'It is a question. Money buys you a lot of things, but not enough to buy happiness.',
+    'What is the best way to buy happiness ?'
+]
+dialog.append("How are you")
+print(dialog)
+response = generate(instruction, knowledge, dialog)
+print(response)
+dialog = [
+    'Does money buy happiness?',
+    'It is a question. Money buys you a lot of things, but not enough to buy happiness.',
+    'What is the best way to buy happiness ?'
+]
+dialog.append('How are you')
+print(dialog)
+response = generate(instruction, knowledge, dialog)
+print(response)
+'''
+def new_text_callback(text: str):
+    print(text, end="", flush=True)
+
+class AudioUtil():
+  # ----------------------------
+  # Load an audio file. Return the signal as a tensor and the sample rate
+  # ----------------------------
+  @staticmethod
+  def open(audio_file):
+    sig, sr = torchaudio.load(audio_file)
+    return (sig, sr)
+  # ----------------------------
+  # Convert the given audio to the desired number of channels
+  # ----------------------------
+  @staticmethod
+  def rechannel(aud, new_channel):
+    sig, sr = aud
+
+    if (sig.shape[0] == new_channel):
+      # Nothing to do
+      return aud
+
+    if (new_channel == 1):
+      # Convert from stereo to mono by selecting only the first channel
+      resig = sig[:1, :]
+    else:
+      # Convert from mono to stereo by duplicating the first channel
+      resig = torch.cat([sig, sig])
+
+    return ((resig, sr))
+  # ----------------------------
+  # Since Resample applies to a single channel, we resample one channel at a time
+  # ----------------------------
+  @staticmethod
+  def resample(aud, newsr):
+    sig, sr = aud
+
+    if (sr == newsr):
+      # Nothing to do
+      return aud
+
+    num_channels = sig.shape[0]
+    # Resample first channel
+    resig = torchaudio.transforms.Resample(sr, newsr)(sig[:1,:])
+    if (num_channels > 1):
+      # Resample the second channel and merge both channels
+      retwo = torchaudio.transforms.Resample(sr, newsr)(sig[1:,:])
+      resig = torch.cat([resig, retwo])
+    return ((resig, newsr))
+  # ----------------------------
+  # Pad (or truncate) the signal to a fixed length 'max_ms' in milliseconds
+  # ----------------------------
+  @staticmethod
+  def pad_trunc(aud, max_ms):
+    sig, sr = aud
+    num_rows, sig_len = sig.shape
+    max_len = sr//1000 * max_ms
+
+    if (sig_len > max_len):
+      # Truncate the signal to the given length
+      sig = sig[:,:max_len]
+
+    elif (sig_len < max_len):
+      # Length of padding to add at the beginning and end of the signal
+      pad_begin_len = random.randint(0, max_len - sig_len)
+      pad_end_len = max_len - sig_len - pad_begin_len
+
+      # Pad with 0s
+      pad_begin = torch.zeros((num_rows, pad_begin_len))
+      pad_end = torch.zeros((num_rows, pad_end_len))
+
+      sig = torch.cat((pad_begin, sig, pad_end), 1)
+      
+    return (sig, sr)
+  # ----------------------------
+  # Shifts the signal to the left or right by some percent. Values at the end
+  # are 'wrapped around' to the start of the transformed signal.
+  # ----------------------------
+  @staticmethod
+  def time_shift(aud, shift_limit):
+    sig,sr = aud
+    _, sig_len = sig.shape
+    shift_amt = int(random.random() * shift_limit * sig_len)
+    return (sig.roll(shift_amt), sr)
+
+  # ----------------------------
+  # Generate a Spectrogram
+  # ----------------------------
+  @staticmethod
+  def spectro_gram(aud, n_mels=64, n_fft=1024, hop_len=None):
+    sig,sr = aud
+    top_db = 80
+
+    # spec has shape [channel, n_mels, time], where channel is mono, stereo etc
+    spec = transforms.MelSpectrogram(sr, n_fft=n_fft, hop_length=hop_len, n_mels=n_mels)(sig)
+
+    # Convert to decibels
+    spec = transforms.AmplitudeToDB(top_db=top_db)(spec)
+    return (spec)
+
+
+  # ----------------------------
+  # Augment the Spectrogram by masking out some sections of it in both the frequency
+  # dimension (ie. horizontal bars) and the time dimension (vertical bars) to prevent
+  # overfitting and to help the model generalise better. The masked sections are
+  # replaced with the mean value.
+  # ----------------------------
+  @staticmethod
+  def spectro_augment(spec, max_mask_pct=0.1, n_freq_masks=1, n_time_masks=1):
+    _, n_mels, n_steps = spec.shape
+    mask_value = spec.mean()
+    aug_spec = spec
+
+    freq_mask_param = max_mask_pct * n_mels
+    for _ in range(n_freq_masks):
+      aug_spec = transforms.FrequencyMasking(freq_mask_param)(aug_spec, mask_value)
+
+    time_mask_param = max_mask_pct * n_steps
+    for _ in range(n_time_masks):
+      aug_spec = transforms.TimeMasking(time_mask_param)(aug_spec, mask_value)
+
+    return aug_spec
+
+
+
+
+# ----------------------------
+# Sound Dataset
+# ----------------------------
+class SoundDS(Dataset):
+  def __init__(self, df, data_path):
+    self.df = df
+    self.data_path = str(data_path)
+    self.duration = 5000
+    self.sr = 44100
+    self.channel = 2
+    self.shift_pct = 0.4
+            
+  # ----------------------------
+  # Number of items in dataset
+  # ----------------------------
+  def __len__(self):
+    return len(self.df)    
+    
+  # ----------------------------
+  # Get i'th item in dataset
+  # ----------------------------
+  def __getitem__(self, idx):
+    # Absolute file path of the audio file - concatenate the audio directory with
+    # the relative path
+    audio_file = self.data_path + self.df.loc[idx, 'relative_path']
+    # Get the Class ID
+    class_id = self.df.loc[idx, 'classID']
+
+    aud = AudioUtil.open(audio_file)
+    # Some sounds have a higher sample rate, or fewer channels compared to the
+    # majority. So make all sounds have the same number of channels and same 
+    # sample rate. Unless the sample rate is the same, the pad_trunc will still
+    # result in arrays of different lengths, even though the sound duration is
+    # the same.
+    reaud = AudioUtil.resample(aud, self.sr)
+    rechan = AudioUtil.rechannel(reaud, self.channel)
+
+    dur_aud = AudioUtil.pad_trunc(rechan, self.duration)
+    shift_aud = AudioUtil.time_shift(dur_aud, self.shift_pct)
+    sgram = AudioUtil.spectro_gram(shift_aud, n_mels=64, n_fft=1024, hop_len=None)
+    aug_sgram = AudioUtil.spectro_augment(sgram, max_mask_pct=0.1, n_freq_masks=2, n_time_masks=2)
+
+    return aug_sgram, class_id
+
+
+
+class AudioClassifier (torch.nn.Module):
+    # ----------------------------
+    # Build the model architecture
+    # ----------------------------
+    def __init__(self):
+        super().__init__()
+        conv_layers = []
+
+        # First Convolution Block with Relu and Batch Norm. Use Kaiming Initialization
+        self.conv1 = nn.Conv2d(2, 8, kernel_size=(5, 5), stride=(2, 2), padding=(2, 2))
+        self.relu1 = nn.ReLU()
+        self.bn1 = nn.BatchNorm2d(8)
+        init.kaiming_normal_(self.conv1.weight, a=0.1)
+        self.conv1.bias.data.zero_()
+        conv_layers += [self.conv1, self.relu1, self.bn1]
+
+        # Second Convolution Block
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
+        self.relu2 = nn.ReLU()
+        self.bn2 = nn.BatchNorm2d(16)
+        init.kaiming_normal_(self.conv2.weight, a=0.1)
+        self.conv2.bias.data.zero_()
+        conv_layers += [self.conv2, self.relu2, self.bn2]
+
+        # Second Convolution Block
+        self.conv3 = nn.Conv2d(16, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
+        self.relu3 = nn.ReLU()
+        self.bn3 = nn.BatchNorm2d(32)
+        init.kaiming_normal_(self.conv3.weight, a=0.1)
+        self.conv3.bias.data.zero_()
+        conv_layers += [self.conv3, self.relu3, self.bn3]
+
+        # Second Convolution Block
+        self.conv4 = nn.Conv2d(32, 64, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
+        self.relu4 = nn.ReLU()
+        self.bn4 = nn.BatchNorm2d(64)
+        init.kaiming_normal_(self.conv4.weight, a=0.1)
+        self.conv4.bias.data.zero_()
+        conv_layers += [self.conv4, self.relu4, self.bn4]
+
+        # Linear Classifier
+        self.ap = nn.AdaptiveAvgPool2d(output_size=1)
+        self.lin = nn.Linear(in_features=64, out_features=10)
+
+        # Wrap the Convolutional Blocks
+        self.conv = nn.Sequential(*conv_layers)
+ 
+    # ----------------------------
+    # Forward pass computations
+    # ----------------------------
+    def forward(self, x):
+        # Run the convolutional blocks
+        x = self.conv(x)
+
+        # Adaptive pool and flatten for input to linear layer
+        x = self.ap(x)
+        x = x.view(x.shape[0], -1)
+
+        # Linear layer
+        x = self.lin(x)
+
+        # Final output
+        return x    
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def predict(model):
+    duration = 5000
+    sr = 44100
+    channel = 2
+    shift_pct = 0.4
+    AudioSegment.from_wav("recording1.wav").export("evaluate.mp3", format="mp3")
+    audio_file = "evaluate.mp3"
+    audio_file = audio_file
+    aud = AudioUtil.open(audio_file)
+    # Some sounds have a higher sample rate, or fewer channels compared to the
+    # majority. So make all sounds have the same number of channels and same 
+    # sample rate. Unless the sample rate is the same, the pad_trunc will still
+    # result in arrays of different lengths, even though the sound duration is
+    # the same.
+    reaud = AudioUtil.resample(aud, sr)
+    rechan = AudioUtil.rechannel(reaud, channel)
+
+    dur_aud = AudioUtil.pad_trunc(rechan, duration)
+    shift_aud = AudioUtil.time_shift(dur_aud, shift_pct)
+    sgram = AudioUtil.spectro_gram(shift_aud, n_mels=64, n_fft=1024, hop_len=None)
+    aug_sgram = AudioUtil.spectro_augment(sgram, max_mask_pct=0.1, n_freq_masks=2, n_time_masks=2)
+    inputs = aug_sgram.to(device)
+    inputs_m, inputs_s = inputs.mean(), inputs.std()
+    inputs = (inputs - inputs_m) / inputs_s
+    outputs = model(inputs.unsqueeze(0))
+    _, prediction = torch.max(outputs,1)
+    result = prediction[0]
+    return result
+
+
+
+
+
+print(torch.__version__)
+print(torchaudio.__version__)
+# Model class must be defined somewhere
+eval_model = torch.load(("/home/sjhjrok/MEGA/School/AI/Project/alzheimer/audio/model"))
+eval_model.eval()
+eval_model = eval_model.to(device)
+r = sr.Recognizer()
+#model = Model(ggml_model='/home/sjhjrok/MEGA/School/AI/Project/gpt4all/chat/gpt4all-ggml.bin', n_ctx=512)
+
+# Sampling frequency
+frequency = 44100
+ 
+# Recording duration in seconds
+duration = 5
+conversation = []
+while True:
+# to record audio from
+# sound-device into a Numpy
+    recording = sd.rec(int(duration * frequency),
+                   samplerate = frequency, channels = 2)
+ 
+# Wait for the audio to complete
+    print("please speak")
+    sd.wait()
+ 
+# using scipy to save the recording in .wav format
+# This will convert the NumPy array
+# to an audio file with the given sampling frequency
+    write("recording0.wav", frequency, recording)
+ 
+# using wavio to save the recording in .wav format
+# This will convert the NumPy array to an audio
+# file with the given sampling frequency
+    wv.write("recording1.wav", recording, frequency, sampwidth=2)
+
+
+
+    with sr.AudioFile("recording1.wav") as source:
+        audio = r.record(source)
+
+    try:
+        s = r.recognize_google(audio)
+        print("Text: "+s)
+#        generated_text = model.generate(s, n_predict=55)
+#       print(generated_text)
+        conversation.append(s)
+        response = generate(instruction, knowledge, conversation)
+        print(response)
+        engine.say(response)
+        engine.runAndWait()
+        print("Predition: "+ str(predict(eval_model)))
+    except Exception as e:
+        print("Exception: "+str(e))
+
+
+#model.generate("Once upon a time, ", n_predict=55, new_text_callback=new_text_callback, n_threads=8)
+
